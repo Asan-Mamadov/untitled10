6799e76f56db50510a61f52df02b3eacf2cf8dfe
mamad <mamadovasan660@gmail.com>
2025-03-26 00:15:35 +0100

    Java work

---
+++ src/Methods.java
@@ -0,0 +1,18 @@
+
+public class Methods {
+    public static void main(String[] args) {
+        numm(" asan ");
+        int age1 = age(23);
+        System.out.println("Жашым " + age1);
+    }
+
+    public static void numm(String name) {
+        System.out.println("Салам" + name);
+
+    }
+
+    public static int age(int b) {
+
+        return b;
+    }
+}
